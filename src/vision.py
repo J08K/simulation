5aebc79f513e02ec435b25a40e3bf53bc86c3ae3
Job van der Wal <48634934+J08K@users.noreply.github.com>
2022-11-23 22:28:26 +0100

    Update vision.py

--- src/vision.py
+++ src/vision.py
@@ -1,5 +1,16 @@
 import math
 
+
+def degrees_to_radians(degrees : float) -> float:
+    return degrees * ( math.pi / 180 )
+
+def radians_to_slope(radians : float) -> float:
+    return math.tan(radians)
+
+def degrees_to_slope(degrees : float) -> float:
+    return radians_to_slope(degrees_to_radians(degrees))
+
+
 def clamp(min_n : int, max_n : int, n : int) -> int:
     n = max(n, min_n)
     n = min(n, max_n)
@@ -11,23 +22,34 @@ def get_max_values(entity_x : int, entity_y : int, view_range : float, board_max
     view_max_x = clamp(0, board_max_x, entity_x + view_range)
     view_min_x = clamp(0, board_max_x, entity_x - view_range)
 
-    view_max_y = clamp(0, board_max_y, entity_y - view_range)
-    view_min_y = clamp(0, board_max_y, entity_y + view_range)
+    view_max_y = clamp(0, board_max_y, entity_y + view_range)
+    view_min_y = clamp(0, board_max_y, entity_y - view_range)
     
     return view_min_x, view_max_x, view_min_y, view_max_y
 
 def in_view_circle(x : int, y : int, r : float, a : float) -> bool:
     # 'a' in radians.
     
+    # Formula based on https://www.desmos.com/calculator/kux33fukmq
+    cutoff = a * x
+
+    y = -y if y < 0 else y # * So we don't get any negative y_numbers.
+    
+    main_formula = math.sqrt(-x ** 2 + r ** 2) - y >= 0
+    secondary = y <= cutoff if a > 0 else y >= cutoff
+
+    # TODO in_view_circle(-1, 1, 3, degrees_to_slope(135)) should == True
+
+    return main_formula and secondary
     
 
 def get_seeable_coord(entity_x : int, entity_y : int, view_range : float, view_fov_degrees : float, board_max_x : int, board_max_y : int, entity_rotation : int) -> list[tuple[int, int]]:
 
     view_min_x, view_max_x, view_min_y, view_max_y = get_max_values(entity_x, entity_y, view_range, board_max_x, board_max_y)
-    
+
     coords = []
     
-    view_fov_radians = math.tan(view_fov_degrees * (math.pi / 180))
+    cutoff_slope = degrees_to_slope(view_fov_degrees)
     
     # TODO Add ability to rotate.
     
@@ -35,4 +57,6 @@ def get_seeable_coord(entity_x : int, entity_y : int, view_range : float, view_f
         
         for y in range(view_min_y, view_max_y + 1):
             
-            is_in_view = math.sqrt(-x ** 2 + view_range ** 2)
+            is_in_view = in_view_circle(x, y, view_range, cutoff_slope)
+
+            # TODO Finish this... I don't really remember exactly what I was doing :P
