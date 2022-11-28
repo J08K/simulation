e4b474df8aaa57c9bfc5ff337d358557e35f22f2
Job van der Wal <j08k0l@gmail.com>
2022-11-28 12:02:29 +0100

    Working on entity locations

--- src/board.py
+++ src/board.py
@@ -1,12 +1,16 @@
+from entities import Entity
 class Board:
 
     __width : int
     __height : int
     __num_sectors : int
+    
+    entities : dict[Entity, tuple[float, float]]
 
     def __init__(self, board_width : int, board_height : int, amount_of_sectors : int) -> None:
         self.__width = board_width
         self.__height = board_height
-        self.__num_sectors = amount_of_sectors
 
-        # TODO Figure out a way to store entity locations.
+        # * For now just storing all entities and their locations.
+        # * This requires looping over every entity...
+        # ! Not very efficient.
