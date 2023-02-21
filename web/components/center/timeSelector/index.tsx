9def6bd874841fe545face94cee89c5c45f883d8
Job van der Wal <j08k0l@gmail.com>
2023-02-21 16:38:40 +0100

    Small Increases in td in UI now possible

--- web/components/center/timeSelector/index.tsx
+++ web/components/center/timeSelector/index.tsx
@@ -1,5 +1,13 @@
-import { ChangeEvent, useEffect, useState } from "react";
+import { ChangeEvent, useState } from "react";
 import styles from "./styles.module.scss";
+
+function getNumDecimals(n : number) {
+    return (n % 1).toString().substring(2).length;
+}
+
+function round(val : number, num_decimals : number) {
+    return Math.round(val * Math.pow(10, num_decimals)) / Math.pow(10, num_decimals);
+}
 
 const TimeSelector = (props : {
     target_time_setter : Function,
@@ -8,6 +16,9 @@ const TimeSelector = (props : {
 
     let time_out : NodeJS.Timeout;
     let [selected_time, setSelectedTime] = useState(0.0);
+
+    let delta_step = props.time_limits.time_delta ? props.time_limits.time_delta : 0.1;
+    let num_decimals = getNumDecimals(delta_step)
 
     function handleSelectLatest(event : ChangeEvent<HTMLInputElement>) {
         let range_selector = document.getElementById("range_selector");
@@ -21,20 +32,37 @@ const TimeSelector = (props : {
         props.target_time_setter(time)
     }
 
-    function handleTimeChange(event : ChangeEvent<HTMLInputElement>) {
-        setSelectedTime(Number(event.target.value));
+    function timeChangeThrottle(time : number) {
+        setSelectedTime(time);
         if (!time_out) {
-            time_out = setTimeout(changeTime, 1000, Number(event.target.value));
+            time_out = setTimeout(changeTime, 1000, time);
         }
         clearTimeout(time_out);
-        time_out = setTimeout(changeTime, 1000, Number(event.target.value));
+        time_out = setTimeout(changeTime, 1000, time);
+    }
+
+    function handleTimeChange(event : ChangeEvent<HTMLInputElement>) {
+        timeChangeThrottle(Number(event.target.value))
+    }
+
+    function handleAddValue(delta_value : number) {
+        let range_selector = document.getElementById("range_selector");
+        if (range_selector instanceof HTMLInputElement) {
+            let new_value = round(Number(range_selector.value) + delta_value, num_decimals);
+            range_selector.value = new_value.toString();
+            timeChangeThrottle(new_value);
+        }
     }
 
     return (
         <div className={styles.TimeSelector}>
             <div className={styles.Header}>Time selection</div>
             <div className={styles.Control}>
-                <input id="range_selector" type="range" onChange={handleTimeChange} defaultValue={selected_time} max={props.time_limits.time_max} step={props.time_limits.time_delta ? props.time_limits.time_delta : 0.1}></input>
+                <div className={styles.RangeSelector}>
+                    <button onClick={() => {handleAddValue(-delta_step)}}>&lt;</button>
+                    <input id="range_selector" type="range" onChange={handleTimeChange} defaultValue={selected_time} max={props.time_limits.time_max} step={delta_step}></input>
+                    <button onClick={() => {handleAddValue(delta_step)}}>&gt;</button>
+                </div>
                 <div>Selected time: <span className={styles.Green}>{selected_time}</span></div>
                 <div className={styles.LatestSelector}>
                     <input id="select_latest" type="checkbox" onChange={handleSelectLatest}/>
