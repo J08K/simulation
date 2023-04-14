36129f6b92672ccc62cc84019e172f7ee797154c
Job van der Wal <j08k0l@gmail.com>
2023-04-14 10:52:48 +0200

    yes

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simulation
+
+This is the code for a school project.
+
+**DO NOT USE** since this currently contains bad security practices. :P
--- analyze/__main__.py
+++ analyze/__main__.py
@@ -9,7 +9,7 @@ PORT = 3001
 USERNAME = "admin"
 PASSWORD = "admin"
 DB_NAME = "simulationdb"
-FULL_URI = db_url = f"mongodb://{USERNAME}:{PASSWORD}@{URI}:{PORT}"
+FULL_URI = f"mongodb://{USERNAME}:{PASSWORD}@{URI}:{PORT}"
 
 BASE_COLLECTION_NAME = "simdata"
 
